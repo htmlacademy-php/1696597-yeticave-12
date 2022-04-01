1ac4cceaa5af1a42c3e453e9dddabf7b85985e2e
Andrei Rotar <andrei.rotari.work@gmail.com>
2022-04-01 22:47:26 +0300

    Added timer to lot cards on the main page
    Added timer to lot cards on the main page using the function 'get_time_to_expiry()' in index php, also added timer--finishing class to highlight the timer if less than an hour remaining till the expiry time

--- index.php
+++ index.php
@@ -16,37 +16,43 @@ $lots = [
         'name' => '2014 Rossignol District Snowboard',
         'category' => $categories[0],
         'price' => 10999,
-        'img_url' => 'img/lot-1.jpg'
+        'img_url' => 'img/lot-1.jpg',
+        'expiry_date' => '2022-04-01 22:25'
     ],
     [
         'name' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => $categories[0],
         'price' => 159999,
-        'img_url' => 'img/lot-2.jpg'
+        'img_url' => 'img/lot-2.jpg',
+        'expiry_date' => '2022-04-01 23:27'
     ],
     [
         'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => $categories[1],
         'price' => 8000,
-        'img_url' => 'img/lot-3.jpg'
+        'img_url' => 'img/lot-3.jpg',
+        'expiry_date' => '2022-04-02 23:32'
     ],
     [
         'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => $categories[2],
         'price' => 10999,
-        'img_url' => 'img/lot-4.jpg'
+        'img_url' => 'img/lot-4.jpg',
+        'expiry_date' => '2022-04-04 15:47'
     ],
     [
         'name' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => $categories[3],
         'price' => 7500,
-        'img_url' => 'img/lot-5.jpg'
+        'img_url' => 'img/lot-5.jpg',
+        'expiry_date' => '2022-04-05 03:20'
     ],
     [
         'name' => 'Маска Oakley Canopy',
         'category' => $categories[5],
         'price' => 5400,
-        'img_url' => 'img/lot-6.jpg'
+        'img_url' => 'img/lot-6.jpg',
+        'expiry_date' => '2022-04-07'
     ],
 ];
 
@@ -59,6 +65,26 @@ function format_price($lot_price)
         $displayed_price = number_format($rounded_price, 0, ',', ' ');
     }
     return $displayed_price . ' ₽';
+}
+
+function get_time_to_expiry($str_expiry_date)
+{
+    $now = date('Y-m-d H:i');
+    $expiry_date = date($str_expiry_date);
+
+    $diff = strtotime($expiry_date) - strtotime($now);
+    $hours = floor($diff/(60*60));
+    $minutes = floor(($diff - ($hours*60*60))/60); 
+    $timer = [$hours, $minutes];
+    
+    // padding single-digit timer values to get double digits, i.e. '0:8' to '00:08'
+    foreach ($timer as $key => $value) {
+        if ($value < 0) {
+            $value = 0;
+        }
+        $timer[$key] = str_pad(strval($value), 2, '0', STR_PAD_LEFT);
+    }
+    return $timer;
 }
 
 $content = include_template('main.php', ['categories' => $categories, 'lots' => $lots]);
