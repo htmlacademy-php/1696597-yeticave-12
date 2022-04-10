c003c5d4687f7e427cddb1d9a913a6ebc5b2d64b
Andrei Rotar <andrei.rotari.work@gmail.com>
2022-04-10 11:23:03 +0300

    Refactored as per reviewer's comments
    Refactored sql code as per reviewer's comments: added explicit NOT NULL constraints, gave tables and columns more suitable names

--- schema.sql
+++ schema.sql
@@ -5,53 +5,51 @@ CREATE DATABASE yeticave
 USE yeticave;
 
 CREATE TABLE users(
-  user_id INT PRIMARY KEY AUTO_INCREMENT,
-  registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   email VARCHAR(50) NOT NULL UNIQUE,
   username VARCHAR(50) NOT NULL UNIQUE,
-  user_password VARCHAR(255),
-  contacts VARCHAR(255)
+  password VARCHAR(255) NOT NULL,
+  contacts VARCHAR(255) NOT NULL
 );
 
 CREATE UNIQUE INDEX user_email_idx ON users(email);
 CREATE UNIQUE INDEX user_name_idx ON users(username);
 
-CREATE TABLE category(
-  category_id INT PRIMARY KEY AUTO_INCREMENT,
-  name_category VARCHAR(30) NOT NULL UNIQUE,
-  code_category VARCHAR(30) NOT NULL UNIQUE
+CREATE TABLE categories(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL UNIQUE,
+  code VARCHAR(30) NOT NULL UNIQUE
 );
 
-CREATE UNIQUE INDEX category_name_idx ON category(name_category);
-CREATE UNIQUE INDEX category_code_idx ON category(code_category);
+CREATE UNIQUE INDEX category_name_idx ON categories(name);
+CREATE UNIQUE INDEX category_code_idx ON categories(code);
 
-CREATE TABLE lot(
-  lot_id INT PRIMARY KEY AUTO_INCREMENT,
-  date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  name_lot VARCHAR(50) NOT NULL,
-  description_lot VARCHAR(50),
-  img_url_lot VARCHAR(50),
-  initial_price_lot DECIMAL(10,2),
-  expiry_date_lot TIMESTAMP,
-  bidding_increment DECIMAL(10, 2),
-  
-  user_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-  winner_id INT,
-  FOREIGN KEY (winner_id) REFERENCES users(user_id) ON DELETE SET NULL,
-  category_id INT,
-  FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE SET NULL
+CREATE TABLE lots(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  user_id INT NOT NULL,
+  category_id INT NULL,
+  winner_id INT NULL,
+  name VARCHAR(50) NOT NULL,
+  description VARCHAR(50) NOT NULL,
+  img_url VARCHAR(50) NOT NULL,
+  date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  date_expiry TIMESTAMP NOT NULL,
+  initial_price DECIMAL(10,2) NOT NULL,
+  bidding_increment DECIMAL(10, 2) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (winner_id) REFERENCES users(id) ON DELETE SET NULL,
+  FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
 );
 
-CREATE INDEX lot_name_idx ON lot(name_lot);
+CREATE INDEX lot_name_idx ON lots(name);
 
-CREATE TABLE bid(
-  bid_id INT PRIMARY KEY AUTO_INCREMENT,
-  date_bid TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  price_bid DECIMAL(10,2),
-
-  user_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-  lot_id INT,
-  FOREIGN KEY (lot_id) REFERENCES lot(lot_id) ON DELETE CASCADE
+CREATE TABLE bids(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  user_id INT NOT NULL,
+  lot_id INT NOT NULL,
+  date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (lot_id) REFERENCES lots(id) ON DELETE CASCADE
 );
