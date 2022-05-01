8c55b90110a304ae13aaf34685d194b4617232a2
Andrei Rotar <andrei.rotari.work@gmail.com>
2022-05-01 13:01:55 +0300

    Removed indentations after PHP tags

--- 404.php
+++ 404.php
@@ -1,6 +1,5 @@
 <?php
-    require('helpers.php');
-    $not_found_template = include_template('404.php');
-    print $not_found_template;
 
-
+require('helpers.php');
+$not_found_template = include_template('404.php');
+print $not_found_template;
